e70950c8edb474a0918f5642b6678c876cd09b25
Giovanni Filice <giovanni.filice@icloud.com>
2022-10-25 18:30:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-import _thread
+import threading 
 from library.Logging import *
 from library.openshift import *
 
 
 def main():
-    _thread.start_new_thread(watch_nodes, ("WatchNodes", "Node"))
-    _thread.start_new_thread(get_nodes, ("GetNodes", "Node"))
+    threading.Thread(target=watch_nodes, args=(("WatchNodes", "Node")), daemon=True, name='WatchNodes')
+    threading.Thread(target=get_nodes, args=(("GetNodes", "Node")), daemon=True, name='GetNodes')
+
 
 #    Logging.logger.info(f"Inizio la verifica degli host fisici di RHV su sui si trovano i nodi Openshift")
 #    _thread.start_new_thread(watch_nodes, ("WatchNodes", 2, "Node"))
