972add8873dd6f84e0b7fa6c3e72c2a1312ec785
Giovanni Filice <giovanni.filice@icloud.com>
2022-10-25 18:58:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ from library.openshift import *
 
 
 def main():
-    t1 = threading.Thread(target=watch_nodes, args=(("Node")), daemon=True, name='WatchNodes')
+    t1 = threading.Thread(target=watch_nodes, args=("Node"), daemon=True, name='WatchNodes')
     t2 = threading.Thread(target=get_nodes, args=("Node"), daemon=True, name='GetNodes')
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-    print("Main thread name: {}".format(threading.main_thread().name))
-    Logging.logger.debug(f"Main thread name: { threading.main_thread().name }")
 if __name__ == "__main__":
     main()
 
