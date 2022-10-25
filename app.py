ff0d5fc260247c8f0d920f3b21b0ef49c532c752
Giovanni Filice <giovanni.filice@icloud.com>
2022-10-25 18:52:30 +0200

    f

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ from library.openshift import *
 
 
 def main():
-    t1 = threading.Thread(target=watch_nodes, args=(("WatchNodes", "Node")), daemon=True, name='WatchNodes')
-    t2 = threading.Thread(target=get_nodes, args=(("GetNodes", "Node")), daemon=True, name='GetNodes')
+    t1 = threading.Thread(target=watch_nodes, args=(("Node")), daemon=True, name='WatchNodes')
+    t2 = threading.Thread(target=get_nodes, args=("Node"), daemon=True, name='GetNodes')
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-#    Logging.logger.info(f"Inizio la verifica degli host fisici di RHV su sui si trovano i nodi Openshift")
-#    _thread.start_new_thread(watch_nodes, ("WatchNodes", 2, "Node"))
 
 
 if __name__ == "__main__":
--- library/openshift.py
+++ library/openshift.py
@@ -37,29 +37,28 @@ def add_label(kind, name, host, datacenter):
 
 
 
-def watch_nodes(ThreadName, kind):
+def watch_nodes(kind):
     v1_ocp = dyn_client.resources.get(api_version="v1", kind=kind)
     for node in v1_ocp.watch():
         host = get_rhv_hosts( node['object'].metadata.name )
-        Logging.logger.debug(f"{ ThreadName } -  { node['object'].metadata.name } on { host } ")
         if "bernina" in host:
             datacenter = "Bernina"
         if "caracciolo" in host:
-            datacenter = "Caraccialo"       
+            datacenter = "Caraccialo"     
+        Logging.logger.debug(f"{ node['object'].metadata.name } on { host } - {datacenter} ")
         add_label( kind, node["object"].metadata.name, host, datacenter )
-    Logging.logger.debug(f"Ogni nodo è stato correttamente identificato e ho aggiunto la label rhv=nodo fisico")
 
 
-def get_nodes(ThreadName, kind):
+def get_nodes( kind):
     v1_ocp = dyn_client.resources.get(api_version="v1", kind=kind)
     nodes_list = v1_ocp.get()
     for node in nodes_list.items:
         host = get_rhv_hosts( node.metadata.name )
-        Logging.logger.debug(f"{ThreadName } -  { node.metadata.name } on { host } ")
         if "bernina" in host:
             datacenter = "Bernina"
         if "caracciolo" in host:
             datacenter = "Caraccialo"
+        Logging.logger.debug(f"{ node.metadata.name } on { host } - { datacenter } ")
         add_label( kind, node.metadata.name, host, datacenter )
     while True:
         Logging.logger.debug(f"Verifica ogni 10 secondi completata correttamente")
