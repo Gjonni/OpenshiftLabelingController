5ae95fa6c420ec089fbcfd9da698df4acbc782da
Giovanni Filice <giovanni.filice@icloud.com>
2022-10-25 18:13:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import os
-import _thread
+import threading
 from library.Logging import *
 from library.openshift import *
 import sched
 import time
 
 
-def main():
-    scheduler = sched.scheduler(time.time, time.sleep)
+def worker():
     scheduler.enter(3, 1, watch_nodes, ("WatchNodes", "Node"))
     scheduler.enter(3, 1, get_nodes, ("GetNodes", "Node"))
-    scheduler.run()
+    scheduler.run
+    print(scheduler.queue)
+
+def main():
+    t = threading.Thread(target=worker, args=())
+    t.start()
 #    Logging.logger.info(f"Inizio la verifica degli host fisici di RHV su sui si trovano i nodi Openshift")
 #    _thread.start_new_thread(watch_nodes, ("WatchNodes", 2, "Node"))
 
 
 if __name__ == "__main__":
+    scheduler = sched.scheduler(time.time, time.sleep)
     main()
 
 
