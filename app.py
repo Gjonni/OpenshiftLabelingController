e12c5cfc616773a44ee6000f3aa7c43a7370309a
Giovanni Filice <giovanni.filice@icloud.com>
2022-10-25 19:07:18 +0200

    c

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from library.openshift import *
 
 
 def main():
-    threading.Thread(target=watch_nodes, args=("WatchNodes", "Node"), daemon=True, name='WatchNodes').start().join()
-    threading.Thread(target=get_nodes, args=("GetNodes", "Node"), daemon=True, name='GetNodes').start().join()
+    threading.Thread(target=watch_nodes, args=("Node"), daemon=True, name='WatchNodes').start().join()
+    threading.Thread(target=get_nodes, args=("Node"), daemon=True, name='GetNodes').start().join()
 
 
 if __name__ == "__main__":
