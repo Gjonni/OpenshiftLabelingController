6112dbcf50ce97e95f081664d6d4ac9b297c5376
Giovanni Filice <giovanni.filice@icloud.com>
2022-11-07 16:13:26 +0100

    test scheduler

--- app.py
+++ app.py
@@ -10,14 +10,12 @@ from library.openshift import *
 
 
 def main():
-    t2 = threading.Thread(target=get_nodes, args=("GetNodes", "Node"), daemon=True, name='GetNodes')
-    t2.start()
-    t2.join()
-
     t1 = threading.Thread(target=watch_nodes, args=("WatchNodes", "Node"), daemon=True, name='WatchNodes')
     t1.start()
     t1.join()
-
+    t2 = threading.Thread(target=get_nodes, args=("GetNodes", "Node"), daemon=True, name='GetNodes')
+    t2.start()
+    t2.join()
 
 if __name__ == "__main__":
     main()
