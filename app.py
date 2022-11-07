cfeb340566b4999343e74fc48ff18356097c2b2e
Giovanni Filice <giovanni.filice@icloud.com>
2022-11-07 16:21:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
     #t2 = threading.Thread(target=get_nodes, args=("GetNodes", "Node"), daemon=True, name='GetNodes')
     #t2.start()
     #t2.join()
-    schedule.every(2).seconds.do(get_nodes, "GetNodes", "Node")
+    schedule.every(10).seconds.do(get_nodes, "GetNodes", "Node")
 
     while True:
         schedule.run_pending()
